f6ebb046bab3cd5c0e851413ca5b68e7415214a4
Patrick Howard Yango <yangohy88@gmail.com>
2023-09-23 11:43:38 +0800

    First Commit of Modal
    Still does not have a carousel and dynamic text

---
+++ src/app.js
@@ -0,0 +1,12 @@
+// app.js
+import React from 'react';
+
+const App = () => {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <h1>Hello!</h1>
+    </div>
+  );
+};
+
+export default App;
